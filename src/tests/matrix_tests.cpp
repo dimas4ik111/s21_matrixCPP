0fd32a9ca6ad20a245721059fbe94f49f0978f69
Osgrey Wilbert <osgreywi@mi-t1.msk.21-school.ru>
2022-10-01 17:45:50 +0300

    test start

--- src/tests/matrix_tests.cpp
+++ src/tests/matrix_tests.cpp
@@ -1,14 +1,18 @@
 #include "../lib/s21_matrix.h"
 #include <gtest/gtest.h>
 
-// #define LLL std::endl
-// #define PRINT std::cout <<
-
-TEST(whatTest, test1) {
-    EXPECT_EQ(2, 2);
+TEST(get_set, test1) {
+    S21Matrix m1(2, 2);
+    m1(1, 1) = 123.456;
+    EXPECT_EQ(m1.get_col(), 2);
+    EXPECT_EQ(m1.get_row(), 2);
+    EXPECT_DOUBLE_EQ(m1(1, 1), 123.456);
 }
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
+int main() {
+    testing::InitGoogleTest();
+    if (RUN_ALL_TESTS()) {
+        std::cout << "ERRORS" << std::endl;
+    }
     return RUN_ALL_TESTS();
 }
